95897220f1a8eb665203a56b8eb6e265ad9a0f1c
production@eurialsm.onmicrosoft.com <production@eurialsm.onmicrosoft.com>
2021-01-04 20:40:01 +0200

    Add project files.

---
+++ Ghidora_Artur_Lab12/Ghidora_Artur_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ghidora_Artur_Lab12.Models;
+
+namespace Ghidora_Artur_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
